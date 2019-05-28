21483a41deb7e6f0b21fc0b284a49e941003cf22
warlesonss <48052114+warlesonss@users.noreply.github.com>
2019-05-28 10:09:02 -0300

    Update INSERT_INTO.sql

--- ENTREGA 2/INSERT_INTO.sql
+++ ENTREGA 2/INSERT_INTO.sql
@@ -6,3 +6,15 @@ INSERT INTO descricao(textopadrao) VALUES('ALTAMENTE SEGURO'),('SEGURO'),('INTER
 
 #INSERÇÃO NA TABELA CIDADES
 #EXECUTAR O ARQUIVO cidades.py
+
+#INSERÇÃO NA TABELA USUARIO
+insert into usuario (id,nome,login,senha,cpf,email) values ('1155',aroldo vargas','aroldovargas','123abc','14925798740','aroldovargas@gmail.com');
+insert into usuario (id,nome,login,senha,cpf,email) values ('1166','alyson gustavo','agustavo','gusta2000','21425498740','alyson_g10@hotmail.com')
+insert into usuario (id,nome,login,senha,cpf,email) values ('1177','warleson sales', 'wsales','w123','12587465210','warleson.sales@gmail.com');
+insert into usuario (id,nome,login,senha,cpf,email) values ('1188','heitor dias','hdias','dias987','24312387650','heitordias@hotmail.com');
+insert into usuario (id,nome,login,senha,cpf,email) values ('1200','moises omena','momena','987123moises','23654898780','moises_2019@hotmail.com');
+insert into usuario (id,nome,login,senha,cpf,email) values ('1211','sergio vidigal','svidigal','sergio777','14520895470','vidigalsergio20@hotmail.com');
+insert into usuario (id,nome,login,senha,cpf,email) values ('1222','juliana silva','jsilva','js1999','98520195773','ju_silva@gmail.com');
+insert into usuario (id,nome,login,senha,cpf,email) values ('1233','amanda nunes','anunes','112358','45621487440','amandanunes@hotmail.com');
+insert into usuario (id,nome,login,senha,cpf,email) values ('1244','barbara gomes','bgomes','barbara1997','43520875419','barbara_g2019@gmail.com');
+insert into usuario (id,nome,login,senha,cpf,email) values ('1255','bruno moreto','bmoreto','bruno123','25013625741','brunomoreto@hotmail.com');
