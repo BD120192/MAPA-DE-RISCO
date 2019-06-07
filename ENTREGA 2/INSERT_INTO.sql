9f95a2769423d6b7ef8d3391a2695a086d2c1cfa
aroldovargas <43659687+aroldovargas@users.noreply.github.com>
2019-06-07 18:14:44 -0300

    Update INSERT_INTO.sql

--- ENTREGA 2/INSERT_INTO.sql
+++ ENTREGA 2/INSERT_INTO.sql
@@ -8,32 +8,22 @@ INSERT INTO descricao(textopadrao) VALUES('ALTAMENTE SEGURO'),('SEGURO'),('INTER
 #EXECUTAR O ARQUIVO cidades.py
 
 #INSERÇÃO NA TABELA USUARIO
-insert into usuario (id,nome,login,senha,cpf,email) values ('1155','aroldo vargas','aroldovargas','123abc','14925798740','aroldovargas@gmail.com');
-insert into usuario (id,nome,login,senha,cpf,email) values ('1166','alyson gustavo','agustavo','gusta2000','21425498740','alyson_g10@hotmail.com')
-insert into usuario (id,nome,login,senha,cpf,email) values ('1177','warleson sales', 'wsales','w123','12587465210','warleson.sales@gmail.com');
-insert into usuario (id,nome,login,senha,cpf,email) values ('1188','heitor dias','hdias','dias987','24312387650','heitordias@hotmail.com');
-insert into usuario (id,nome,login,senha,cpf,email) values ('1200','moises omena','momena','987123moises','23654898780','moises_2019@hotmail.com');
-insert into usuario (id,nome,login,senha,cpf,email) values ('1211','sergio vidigal','svidigal','sergio777','14520895470','vidigalsergio20@hotmail.com');
-insert into usuario (id,nome,login,senha,cpf,email) values ('1222','juliana silva','jsilva','js1999','98520195773','ju_silva@gmail.com');
-insert into usuario (id,nome,login,senha,cpf,email) values ('1233','amanda nunes','anunes','112358','45621487440','amandanunes@hotmail.com');
-insert into usuario (id,nome,login,senha,cpf,email) values ('1244','barbara gomes','bgomes','barbara1997','43520875419','barbara_g2019@gmail.com');
-insert into usuario (id,nome,login,senha,cpf,email) values ('1255','bruno moreto','bmoreto','bruno123','25013625741','brunomoreto@hotmail.com');
+INSERT INTO usuario (nome,login,senha,cpf,email) VALUES ('aroldo vargas','aroldovargas','123abc','14925798740','aroldovargas@gmail.com'),('alyson gustavo','agustavo','gusta2000','21425498740','alyson_g10@hotmail.com'),
+('warleson sales', 'wsales','w123','12587465210','warleson.sales@gmail.com'),('heitor dias','hdias','dias987','24312387650','heitordias@hotmail.com'),('moises omena','momena','987123moises','23654898780','moises_2019@hotmail.com'),
+('sergio vidigal','svidigal','sergio777','14520895470','vidigalsergio20@hotmail.com'),('juliana silva','jsilva','js1999','98520195773','ju_silva@gmail.com'),('amanda nunes','anunes','112358','45621487440','amandanunes@hotmail.com'),
+('barbara gomes','bgomes','barbara1997','43520875419','barbara_g2019@gmail.com'),('bruno moreto','bmoreto','bruno123','25013625741','brunomoreto@hotmail.com');
 
-                                                                                                                                                        
-  # INSERT TIPO_COMENTARIO 
+# INSERT TIPO_COMENTARIO 
+INSERT INTO tipo_comentario (descricao_tipo) VALUES ('SISTEMA'),('RUA'),('LINHA');
 
-insert into tipo_comentario (id, descricao_tipo) values ('12','Faltam serem incluídas as linhas de baldeação');
-insert into tipo_comentario (id, descricao_tipo) values ('13','Incluír uma seção de ocorrências em tempo real');
-insert into tipo_comentario (id, descricao_tipo) values ('14','Maior investimento nas linhas que fazem T.Cariacíca -> T.Vila Velha');
-insert into tipo_comentario (id, descricao_tipo) values ('15','Incluír o Seletivo na lista de ônibus');
+# INSERT COMENTARIO 
+insert into comentario (comentario_usuario,id_usuario,id_tipo_comentario) values ('Faltam serem incluídas as linhas de baldeação');
+insert into comentario (id_usuario) values ('13','Incluír uma seção de ocorrências em tempo real');
+insert into comentario (id, descricao_tipo) values ('14','Maior investimento nas linhas que fazem T.Cariacíca -> T.Vila Velha');
+insert into comentario (id, descricao_tipo) values ('15','Incluír o Seletivo na lista de ônibus');
 
 # INSERT SEXO
+INSERT INTO sexo(genero) VALUES('M'),('F');
 
-insert into sexo (id, genero) values ('1001','F');
-insert into sexo (id, genero) values ('1002','M');
-                                                 
-insert into classificacao (id,tipo,id_descricao) values ('2001','A','1');
-insert into classificacao (id,tipo,id_descricao) values ('2002','B','2');
-insert into classificacao (id,tipo,id_descricao) values ('2003','C','3');
-insert into classificacao (id,tipo,id_descricao) values ('2004','D','4');
-insert into classificacao (id,tipo,id_descricao) values ('2005','E','5');
+# INSERT CLASSIFICACAO
+INSERT INTO classificacao (tipo,id_descricao) VALUES ('A','1'),('B','2'),('C','3'),('D','4'),('E','5');
