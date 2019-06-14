3691226d713163130556c781032ea1b136d2ca4a
aroldovargas <43659687+aroldovargas@users.noreply.github.com>
2019-06-14 12:21:38 -0300

    Update CREATE_TABLE.sql

--- ENTREGA 2/CREATE_TABLE.sql
+++ ENTREGA 2/CREATE_TABLE.sql
@@ -1,6 +1,5 @@
 # As tabelas devem ser criadas nessa sequencia, para que seja possivel criar as chaves estrangeiras e os relacionamentos.
 
-
 #CRIAÇÃO DA TABELA USUARIO
 CREATE TABLE usuario(id serial NOT NULL,nome varchar(100),login varchar(50),senha varchar(16),cpf varchar(11),email varchar(50),PRIMARY KEY(id));
 
@@ -18,7 +17,7 @@ CREATE TABLE descricao (id serial NOT NULL, textopadrao varchar(100) NOT NULL,PR
 CREATE TABLE classificacao (id serial NOT NULL, tipo char(1) NOT NULL,id_descricao serial NOT NULL, PRIMARY KEY(id),FOREIGN KEY (id_descricao) REFERENCES descricao(id));
 
 #CRIAÇÃO DA TABELA CIDADE
-CREATE TABLE cidade (id serial NOT NULL, nome varchar(50),PRIMARY KEY(id));
+CREATE TABLE cidade (id serial NOT NULL, nome_cidade varchar(50),PRIMARY KEY(id));
 
 #CRIAÇÃO DA TABELA BAIRRO
 CREATE TABLE bairro (id serial NOT NULL, nome_bairro varchar(100) NOT NULL,id_cidade serial NOT NULL,PRIMARY KEY(id),FOREIGN KEY (id_cidade) REFERENCES cidade(id));
@@ -43,5 +42,5 @@ CREATE TABLE linha(id serial NOT NULL,numero_linha integer NOT NULL,id_classific
 CREATE TABLE linha_passa_rua(id serial NOT NULL,id_rua serial NOT NULL, id_linha serial NOT NULL,PRIMARY KEY(id),FOREIGN KEY (id_rua) REFERENCES rua(id),FOREIGN KEY (id_linha) REFERENCES linha(id));
 
 #CRIAÇÃO DA TABELA CRIME
-CREATE TABLE crime(id integer NOT NULL,data date,hora time,id_vitima serial,id_rua serial,id_bairro serial,id_cidade serial, id_tipo_crime serial,PRIMARY KEY(id),
+CREATE TABLE crime(id serial NOT NULL,data date,hora time,id_vitima serial,id_rua serial,id_bairro serial,id_cidade serial, id_tipo_crime serial,PRIMARY KEY(id),
 FOREIGN KEY (id_vitima) REFERENCES vitima(id), FOREIGN KEY (id_rua) REFERENCES rua(id), FOREIGN KEY (id_bairro) REFERENCES bairro(id), FOREIGN KEY (id_cidade) REFERENCES cidade(id), FOREIGN KEY (id_tipo_crime) REFERENCES tipo_crime(id));
