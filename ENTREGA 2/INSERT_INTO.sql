81c58884d5504a6ed0d1b49b24ed6f783555599c
warlesonss <48052114+warlesonss@users.noreply.github.com>
2019-06-14 08:15:40 -0300

    Update INSERT_INTO.sql

--- ENTREGA 2/INSERT_INTO.sql
+++ ENTREGA 2/INSERT_INTO.sql
@@ -37,6 +37,8 @@ INSERT INTO bairro (nome_bairro,id_cidade) SELECT cb.bairro,ci.id FROM crimebase
 INNER JOIN cidade ci ON cb.cidade = ci.nome_cidade
 GROUP BY cb.bairro,ci.id;
 
+
+
 # INSERT VITIMA
 #Seleciona a idade e o id_sexo da vitimas.
 SELECT cb.idade,se.id FROM crimebase cb INNER JOIN sexo se ON cb.sexo = se.genero GROUP BY cb.idade,se.id;
@@ -45,3 +47,25 @@ SELECT cb.idade,se.id FROM crimebase cb INNER JOIN sexo se ON cb.sexo = se.gener
 INSERT INTO vitima (idade,id_sexo) SELECT cb.idade,se.id FROM crimebase cb
 INNER JOIN sexo se ON cb.sexo = se.genero
 GROUP BY cb.idade,se.id;
+
+
+#INSERT RUA, AS RUAS NÃO TEM INSERIDAS.
+INSERT INTO rua (nome_rua,id_classificacao) values ('AV DES JOSE A M FEU ROSA',2),('RODOVIA ES 010',2),('RUA E',2),
+('RUA CARIOCA',2),('BR 101',2),('AV FERNANDO FERRARI',2),('RUA DONA MARIA ROSA',2),('AV MARUIPE',2),('AV PAULINHO MULLER',2),('AV VITORIA',2),
+('RUA HENRIQUE NOVAES',2),('AV JERONIMO MONTEIRO',2),('AV REPUBLICA',2),('AV CLETO NUNES',2),('AV MARCOS DE AZEVEDO',2),('AV DUARTE LEMOS',2),
+('AV NAIR DE AZEVEDO SILVA',2),('AV ATERRO DA CONDUSA',2),('AV ALEXANDRE BUAIZ',2),('PONTE FLORENTINO AVIDOS',2),('AV ROBERT KENNEDY',2),
+('RUA MANOEL GILSON DA SILVA',2),('PRAÇA DE SAO TORQUATO',2),('AV GRAÇA ARANHA',2),('RUA MAGNO COUTINHO',2),('RODOVIA CARLOS LINDENBERG',2),
+('AV TRECY CORTELIETE',2),('AV ANA MEROTTO STEFANON',2),('AV JOÃO FRANCISCO GONÇALVES',2),('RUA SÃO LUIZ',2),('RUA D',2),('RUA SÃO DOMINGOS',2),
+('AV NOSSA SENHORA DA PENHA',2),('AV.GETULIO VARGAS',2),('AV ELIAS MIGUEL',2),('RUA GODOFREDO SHNEIDER',2),('AV LUCIANO DAS NEVES',2),('RUA JAGUARIPE',2),
+('RUA HUMBERTO MARTINS DE PAULA',2),('RUA ALAOR DE QUEIRÓS ARAUJO',2),('RUA JOSE TEIXEIRA',2),('RUA SÃO DOMINGOS',2),('RUA D',2),('RUA DOS SABIAS',2),
+('AV.PAULO PEREIRA GOMES',2),('TREVO DE LARANJEIRAS II',2),('RUA A2',2),('AV CIVIT',2),('RODOVIA NORTE SUL',2); 
+
+# INSERT LINHA
+insert into linha (id,numero_linha,id_classificacao) values ('1','501',4) ('2','516',4),('3','860',2);
+
+# INSERT LINHA_PASSA_RUA
+
+insert into linha_passa_rua (id_rua,id_linha) values ('106','2'),('107','2'),('108','2'),('16','2'),('109','2'),('110','2'),('111','2'),('112','2'),('113','2'),('114','2'),('115','2'),
+('116','2'),('117','2'),('118','2'),('119','2'),('120','2'),('121','2'),('122','2'),('123','2'),('124','2'),('125','2'),('126','2'),('127','2'),('128','2'),('129','2'),('130','2'),
+('130','2'),('131','2'),('132','2'),('133','2'),('134','2'),('135','2'),('135','2'),('136','2'),('137','2'),('138','2'),('139','2'),('140','2'),('141','2'),('142','2'),('143','2'),('144','2'),
+('145','1'),('146','1'),('147','1'),('148','1'),('149','1'),('150','1'),('151','1'),('152','3'),('153','3'),('154','3'),('155','3'),('156','3'),('157','3')
