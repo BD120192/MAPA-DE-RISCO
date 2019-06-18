1d80fe38afe2ad047b9da8ab9874fe6bfc2bd042
warlesonss <48052114+warlesonss@users.noreply.github.com>
2019-06-17 23:40:24 -0300

    Update INSERT_INTO.sql

--- ENTREGA 2/INSERT_INTO.sql
+++ ENTREGA 2/INSERT_INTO.sql
@@ -49,23 +49,114 @@ INNER JOIN sexo se ON cb.sexo = se.genero
 GROUP BY cb.idade,se.id;
 
 
-#INSERT RUA, AS RUAS NÃO TEM INSERIDAS.
-INSERT INTO rua (nome_rua,id_classificacao) values ('AV DES JOSE A M FEU ROSA',2),('RODOVIA ES 010',2),('RUA E',2),
-('RUA CARIOCA',2),('BR 101',2),('AV FERNANDO FERRARI',2),('RUA DONA MARIA ROSA',2),('AV MARUIPE',2),('AV PAULINHO MULLER',2),('AV VITORIA',2),
-('RUA HENRIQUE NOVAES',2),('AV JERONIMO MONTEIRO',2),('AV REPUBLICA',2),('AV CLETO NUNES',2),('AV MARCOS DE AZEVEDO',2),('AV DUARTE LEMOS',2),
-('AV NAIR DE AZEVEDO SILVA',2),('AV ATERRO DA CONDUSA',2),('AV ALEXANDRE BUAIZ',2),('PONTE FLORENTINO AVIDOS',2),('AV ROBERT KENNEDY',2),
-('RUA MANOEL GILSON DA SILVA',2),('PRAÇA DE SAO TORQUATO',2),('AV GRAÇA ARANHA',2),('RUA MAGNO COUTINHO',2),('RODOVIA CARLOS LINDENBERG',2),
-('AV TRECY CORTELIETE',2),('AV ANA MEROTTO STEFANON',2),('AV JOÃO FRANCISCO GONÇALVES',2),('RUA SÃO LUIZ',2),('RUA D',2),('RUA SÃO DOMINGOS',2),
-('AV NOSSA SENHORA DA PENHA',2),('AV.GETULIO VARGAS',2),('AV ELIAS MIGUEL',2),('RUA GODOFREDO SHNEIDER',2),('AV LUCIANO DAS NEVES',2),('RUA JAGUARIPE',2),
-('RUA HUMBERTO MARTINS DE PAULA',2),('RUA ALAOR DE QUEIRÓS ARAUJO',2),('RUA JOSE TEIXEIRA',2),('RUA SÃO DOMINGOS',2),('RUA D',2),('RUA DOS SABIAS',2),
-('AV.PAULO PEREIRA GOMES',2),('TREVO DE LARANJEIRAS II',2),('RUA A2',2),('AV CIVIT',2),('RODOVIA NORTE SUL',2); 
+
+# INSERT INTO BAIRRO (nome_bairro,id_cidade) VALUES ('MANGUINHOS',70),('JACARAIPE',70),('GOIABEIRAS',70),('COBILANDIA',77),('PRAIA DA COSTA',77),('SANTA LUCIA',78),('ITAPUA',77),('SANTA HELENA',78),('MORADA DE LARANJEIRAS',70),('MARUIPE',78),('JUCUTUQUARA',98),('LARANJEIRAS',99);
+
+
+INSERT INTO RUA (nome_rua,quantidade_crimes,id_classificacao,id_bairro,id_cidade) VALUES
+
+('AV JERONIMO MONTEIRO','0','1','21','78'),('AV DES JOSE A M FEU ROSA','0','1','87','70'),('RODOVIA ES 010','0','87','1'),('RUA E','0','1','89','70'),
+('RUA CARIOCA','0','1','44','70'),('BR 101','0','1','44','70'),('AV FERNANDO FERRARI','0','1','90','78'),('RUA DONA MARIA ROSA','0','1','37','78'),('AV MARUIPE','0','1','97','78'),('AV PAULINHO MULLER','0','1','98','78'),('AV VITORIA','0','1','98','78'),
+('RUA HENRIQUE NOVAES','0','1','21','78'),('AV REPUBLICA','0','1','21','78'),('AV CLETO NUNES','0','1','21','78'),('AV MARCOS DE AZEVEDO','0','1','8','78'),('AV DUARTE LEMOS','0','1','8','78'),
+('AV NAIR DE AZEVEDO SILVA','0','1','8','78'),('AV ATERRO DA CONDUSA','0','1','8','78'),('AV ALEXANDRE BUAIZ','0','1','27','78'),('PONTE FLORENTINO AVIDOS','0','1','27','78'),('AV ROBERT KENNEDY','0','1','37','78'),('RUA MANOEL GILSON DA SILVA','0','1','16','77'),
+('PRAÇA DE SAO TORQUATO','0','1','16','77'),('AV GRAÇA ARANHA','0','1','16','77'),('RUA MAGNO COUTINHO','0','1','16','77'),('AV CARLOS LINDENBERG','1','1','61','77'),
+('AV TRECY CORTELIETE','0','1','61','77'),('AV ANA MEROTTO STEFANON','0','1','91',',77'),('AV JOÃO FRANCISCO GONÇALVES','0','1','91','77'),('RUA SÃO LUIZ','0','1','92','77'),('RUA D','0','1','4','70'),('RUA SÃO DOMINGOS','0','1','26','70'),
+('AV NOSSA SENHORA DA PENHA','0','1','93','78'),('AV.GETULIO VARGAS','1','1','65','77'),('AV ELIAS MIGUEL','0','1','65','77'),('RUA GODOFREDO SHNEIDER','0','1','36','77'),('AV LUCIANO DAS NEVES','0','1','92','77'),('RUA JAGUARIPE','0','1','94','77'),
+('RUA HUMBERTO MARTINS DE PAULA','0','1','95','78'),('RUA ALAOR DE QUEIROS ARAUJO','0','1','95','78'),('RUA JOSE TEIXEIRA','0','1','95','78'),('RUA SÃO DOMINGOS','0','1','4','70'),('RUA D','0','1','4','70'),('RUA DOS SABIAS','0','1','4','70'),
+('AV PAULO PEREIRA GOMES','0','1','96','70'),('TREVO DE LARANJEIRAS II','0','1','99','70'),('RUA A2','0','1','99','70'),('AV CIVIT','0','1','99','70'),('RODOVIA NORTE SUL','0','1','99','70');
 
 # INSERT LINHA
 insert into linha (id,numero_linha,id_classificacao) values ('1','501',4) ('2','516',4),('3','860',2);
 
 # INSERT LINHA_PASSA_RUA
 
-insert into linha_passa_rua (id_rua,id_linha) values ('106','2'),('107','2'),('108','2'),('16','2'),('109','2'),('110','2'),('111','2'),('112','2'),('113','2'),('114','2'),('115','2'),
-('116','2'),('117','2'),('118','2'),('119','2'),('120','2'),('121','2'),('122','2'),('123','2'),('124','2'),('125','2'),('126','2'),('127','2'),('128','2'),('129','2'),('130','2'),
-('130','2'),('131','2'),('132','2'),('133','2'),('134','2'),('135','2'),('135','2'),('136','2'),('137','2'),('138','2'),('139','2'),('140','2'),('141','2'),('142','2'),('143','2'),('144','2'),
-('145','1'),('146','1'),('147','1'),('148','1'),('149','1'),('150','1'),('151','1'),('152','3'),('153','3'),('154','3'),('155','3'),('156','3'),('157','3')
+insert into linha_passa_rua (id_rua,id_linha) values insert into linha_passa_rua (id_rua,id_linha) values ('16','2'),('103','2'),('104','2'),('105','2'),('106','2'),('107','2'),('108','2'),('109','2'),('110','2'),('111','2'),('112','2'),
+('113','2'),('114','2'),('115','2'),('116','2'),('117','2'),('118','2'),('119','2'),('120','2'),('121','2'),('122','2'),('123','2'),('124','2'),('125','2'),('126','2'),('127','2'),
+('128','2'),('129','2'),('130','2'),('131','2'),('132','2'),('133','2'),('134','2'),('135','2'),('136','2'),('137','2'),('138','2'),('139','2'),('140','2'),('141','2'),('142','2'),('143','2'),
+('144','1'),('145','1'),('146','1'),('147','1'),('148','1'),('149','1'),('150','1'),('151','3'),('152','3'),('153','3'),('154','3'),('155','3'),('156','3')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
